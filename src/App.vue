e86de70ac0f60235820b510434225b7c2b0b4522
Fran <francocaminosmail@gmail.com>
2023-01-31 12:12:06 -0300

    project added

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <v-app>
+    <v-main>
+      <v-container>
+        <router-view />
+
+        <v-dialog v-model="loading.estado" hide-overlay persistent width="300">
+          <v-card color="primary" dark>
+            <v-card-text :color="loading.color" dark>
+              {{ loading.titulo }}
+              <v-progress-linear
+                indeterminate
+                color="white"
+                class="mb-0"
+              ></v-progress-linear>
+            </v-card-text>
+          </v-card>
+        </v-dialog>
+      </v-container>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+import { mapState } from 'vuex';
+
+export default {
+  name: "App",
+
+  data: () => ({
+    //
+  }),
+  computed:{
+    ...mapState(['loading'])
+  }
+};
+</script>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,78 @@
+<template>
+  <div>
+    <v-layout :wrap="true">
+      <v-flex xs12>
+        <v-card>
+          <v-date-picker
+          v-model="fecha"
+          class="mt-4"
+          fullWidth
+          locale="es-AR"
+          :min="minimo"
+          :max="maximo"
+          @change="getDolar(fecha)"
+        ></v-date-picker>
+        </v-card>
+
+        <v-card color="primary" dark>
+          <v-card-text class="display-1 text-center">
+            {{valor}}
+          </v-card-text>
+        </v-card>
+      </v-flex>
+    </v-layout>
+  </div>
+</template>
+
+<script>
+import HelloWorld from "../components/HelloWorld";
+import axios from "axios";
+import { mapMutations } from "vuex";
+
+export default {
+  name: "Home",
+
+  components: {
+    HelloWorld,
+  },
+  data(){
+    return{
+      fecha: new Date().toISOString().substring(0, 10),
+      minimo: '1984',
+      maximo: new Date().toISOString().substring(0, 10),
+      valor: null
+    }
+  },
+  methods: {
+    ...mapMutations(['mostrarLoading', 'ocultarLoading']),
+
+    async getDolar(dia){
+      console.log(dia)
+      let fechaInvertida = dia.split(['-']);
+      let ddmmyy = fechaInvertida[2]+ '-' +fechaInvertida[1] + '-' + fechaInvertida[0];
+
+      try {
+        this.mostrarLoading({titulo:'Cargando', color:'primary'})
+        let dolar = await axios.get(`https://mindicador.cl/api/dolar/${ddmmyy}`);
+        if(dolar.data.serie.length > 0) {
+          this.valor = await dolar.data.serie[0].valor;
+        } else {
+          this.valor = 'Sin resultados'
+        }
+
+
+
+      } catch (error) {
+        console.log(error);
+      } finally {
+        this.ocultarLoading()
+      }
+
+
+    }
+  },
+  created(){
+    this.getDolar(this.fecha)
+  }
+};
+</script>
